9d1dd6ddffa5e8479b1d9964ddf742cee3d07ec9
Namrata Singh <namratasinghvns7@gmail.com>
2020-09-23 11:25:41 +0530

    Update for loop

--- Loops.java
+++ Loops.java
@@ -4,5 +4,9 @@ class Loops{
 
     for (int  i = 0 ; i < 10 ; i++)
        System.out.println("Hello Loop");
+
+
+       System.out.println("Hello Java!!!!");
+       System.out.println("Hello Namrata!!!!");
   }
 }
